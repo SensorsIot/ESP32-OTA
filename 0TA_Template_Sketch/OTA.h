70f77a161491fe07428bd4964a9d0cb50ea7fe1b
WacKEDmaN <35508035+WacKEDmaN@users.noreply.github.com>
2020-05-17 18:06:13 +1000

    Update OTA.h
    added ESP8266 includes
    fixed void issue compiling under ESP8266

--- 0TA_Template_Sketch/OTA.h
+++ 0TA_Template_Sketch/OTA.h
@@ -1,7 +1,14 @@
-#include <WiFi.h>
-#include <ESPmDNS.h>
-#include <WiFiUdp.h>
 #include <ArduinoOTA.h>
+#ifdef ESP32
+#include <ESPmDNS.h>
+#include <WiFi.h>
+#endif
+#ifdef ESP8266
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266mDNS.h>
+#endif
+#include <WiFiUdp.h>
 #include <credentials.h>
 
 const char* ssid = mySSID;
@@ -26,8 +33,7 @@ void setupOTA() {
   // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
   // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
 
-  ArduinoOTA
-    .onStart([]() {
+  ArduinoOTA.onStart([]() {
       String type;
       if (ArduinoOTA.getCommand() == U_FLASH)
         type = "sketch";
@@ -36,14 +42,14 @@ void setupOTA() {
 
       // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
       Serial.println("Start updating " + type);
-    })
-    .onEnd([]() {
+    });
+    ArduinoOTA.onEnd([]() {
       Serial.println("\nEnd");
-    })
-    .onProgress([](unsigned int progress, unsigned int total) {
+    });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
       Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    })
-    .onError([](ota_error_t error) {
+    });
+    ArduinoOTA.onError([](ota_error_t error) {
       Serial.printf("Error[%u]: ", error);
       if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
       else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
