bdf814bf15d6e3ccd4eac6a1c2a3a95debe52306
Vincent Stragier <vincent.stragier@outlook.com>
2020-09-22 00:16:51 +0200

    Resolve FreeRTOS (SensorsIot#8)
    - Improve README.md
    - Correct FreeRTOS task function name and code
    - Dynamically handle the parameters passed to the setupOTA function
    - Explicitly provide the credentials in the sketch
    /!\ the modification have been made on both sketch but have only been tested on the non Telnet code.

--- 0TA_Template_Sketch_TelnetStream/0TA_Template_Sketch_TelnetStream.ino
+++ 0TA_Template_Sketch_TelnetStream/0TA_Template_Sketch_TelnetStream.ino
@@ -1,24 +1,28 @@
+#define ESP32_RTOS  // Uncomment this line if you want to use the code with freertos only on the ESP32
+                    // Has to be done before including "OTA.h"
+
 #include "OTA.h"
+#include <credentials.h>
 
-// #define ESP32_RTOS  // Uncomment this line if you want to use the code with freertos (only works on the ESP32)
-
-unsigned long entry;
+uint32_t entry;
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Booting");
-  setupOTA("TemplateSketch");
-  
+
+  setupOTA("TemplateSketch", mySSID, myPASSWORD);
+
+  // Your setup code
 }
 
 void loop() {
-  entry=micros();
-  
-  #ifndef ESP32_RTOS
+  entry = micros();
+#ifdef defined(ESP32_RTOS) && defined(ESP32)
+#else // If you do not use FreeRTOS, you have to regulary call the handle method.
   ArduinoOTA.handle();
-  #endif
-  
+#endif
   TelnetStream.println(micros()-entry);
   TelnetStream.println("Loop");
   delay(1000);
+  // Your code here
 }
--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # ESP32 OTA
- 
-Material for my YouTube video
+
+Material for my YouTube [video](https://www.youtube.com/watch?v=1pwqS_NUG7Q).
+
+## Installation
+
+In order to use the code provided in this repository you need to provide the credentials of your access point. Or you can comment out the `#include <credentials.h>` and define the mySSID and myPASSWORD to the `setupOTA("TemplateSketch", mySSID, myPASSWORD)` function.
+
+### Provide your credentials
+
+Create a `credentials.h` file (in the sketch folder directly or in the Arduino library folder).
+For Windows , the library folder is located in the same folder as the  sketch (`[...]\Arduino\libraries\Credentials`, `[...]\Arduino`).
+
+The text file `credentials.h` looks like:
+
+```c++
+#pragma once
+const char* mySSID = "SSID of your AP";
+const char* myPASSWORD = "Password of your AP";
+```
